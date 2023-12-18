b464475b12be89be10393ed62ed2ddb1ae4d8f40
Jan T'Sestigh <jan.tsestigh@gmail.com>
2023-12-18 14:54:27 +0100

    Project created

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ReqMan
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
